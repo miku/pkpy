b45c8f49f0d785c3c6b651915baf23f6966b3a15
Martin Czygan <martin.czygan@gmail.com>
2014-05-15 11:12:50 +0200

    caching

--- app.py
+++ app.py
@@ -5,32 +5,44 @@ from gluish.path import iterfiles
 from gluish.utils import shellout
 import logging
 import os
+import shelve
 import tempfile
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
 logger = logging.getLogger('borghq')
 
 app = Flask(__name__)
+CACHE = os.path.join(app.static_folder, '.cache.shelve')
+
 
 @app.route("/github/<username>/<repo>/<target>")
 def build_from_github(username, repo, target):
     if not target in ('deb', 'rpm'):
         return abort(404)
     repo_url = 'git@github.com:%s/%s.git' % (username, repo)
-    print(BANNER)
-    print('Building %s for %s ...' % (target, repo_url))
 
-    stopover = tempfile.mkdtemp(prefix='pypack')
-    shellout("""
-        cd {stopover} && git clone {repo_url} &&
-        cd {repo} && fpm --verbose -s python -t {target} .""",
-             stopover=stopover, repo_url=repo_url, repo=repo, target=target)
-    src = iterfiles(stopover, fun=lambda fn: fn.endswith(target)).next()
-    basename = os.path.basename(src)
-    dst = os.path.join(app.static_folder, basename)
-    shellout('cp {src} {dst}', src=src, dst=dst)
+    cache_key = '%s %s' % (repo_url, target)
+    cache = shelve.open(CACHE)
 
-    return send_from_directory(app.static_folder, basename, as_attachment=True)
+    if not cache_key in cache:
+	logger.debug('Building (%s, %s) ...' % (repo_url, target))
+	stopover = tempfile.mkdtemp(prefix='borghq')
+	shellout("""
+	    cd {stopover} && git clone {repo_url} &&
+	    cd {repo} && fpm --verbose -s python -t {target} .""",
+		 stopover=stopover, repo_url=repo_url, repo=repo, target=target)
+	src = iterfiles(stopover, fun=lambda fn: fn.endswith(target)).next()
+	basename = os.path.basename(src)
+	dst = os.path.join(app.static_folder, basename)
+	shellout('cp {src} {dst}', src=src, dst=dst)
+	cache[cache_key] = basename
+    else:
+	logger.debug('Cache hit...')
+
+    filename = cache[cache_key]
+    cache.close()
+
+    return send_from_directory(app.static_folder, filename, as_attachment=True)
 
 
 @app.route("/pypi/<name>/<target>")
