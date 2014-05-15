2277ad66701731bfb4f01f05e51eac3c6ad20819
Martin Czygan <martin.czygan@gmail.com>
2014-05-15 03:23:57 +0200

    first working version

--- README.md
+++ README.md
@@ -10,3 +10,26 @@ Acts as an DEB/RPM repository. Add these package repositories
 to your system one and start installing python packages
 just like native packages. Boom.
 
+
+Problem
+-------
+
+You have a bunch of python packages for a project and you would like
+to install them natively via package manager, e.g. rpm.
+
+All you have is a `requirements.txt`. You send your `requirements.txt`
+to a service and the service responds.
+
+Option 1: A zip file containing all native packages plus an install
+schcript, that will install the packages **in the right order**.
+Download the zip to your deployment machine, extract and run `./install`
+as root. Done. Pro: No custom repository needed.
+
+Option 2: Set up some package repository.
+
+Option 3: Create a separate package, something like borg. Install
+borg on the deployment machine. Then give borg a unique
+identifier (e.g. URL), and borg will fetch the zip (Option 1),
+extract and install it on its own.
+
+
---
+++ app.py
@@ -0,0 +1,70 @@
+# coding: utf-8
+
+from flask import Flask, render_template, redirect, url_for, abort, send_from_directory
+from gluish.path import iterfiles
+from gluish.utils import shellout
+import logging
+import os
+import tempfile
+
+logging.basicConfig()
+
+BANNER = """
+__/\\\__________________________________________________/\\\_______________________        
+ _\/\\\_________________________________________________\/\\\_______________________       
+  _\/\\\______________________________________/\\\\\\\\__\/\\\___________/\\\\\\\\___      
+   _\/\\\____________/\\\\\_____/\\/\\\\\\\___/\\\////\\\_\/\\\__________/\\\////\\\__     
+    _\/\\\\\\\\\____/\\\///\\\__\/\\\/////\\\_\//\\\\\\\\\_\/\\\\\\\\\\__\//\\\\\\\\\__    
+     _\/\\\////\\\__/\\\__\//\\\_\/\\\___\///___\///////\\\_\/\\\/////\\\__\///////\\\__   
+      _\/\\\__\/\\\_\//\\\__/\\\__\/\\\__________/\\_____\\\_\/\\\___\/\\\________\/\\\__  
+       _\/\\\\\\\\\___\///\\\\\/___\/\\\_________\//\\\\\\\\__\/\\\___\/\\\________\/\\\\_ 
+        _\/////////______\/////_____\///___________\////////___\///____\///_________\////__
+"""
+
+app = Flask(__name__)
+
+@app.route("/github/<username>/<repo>/<target>")
+def build_from_github(username, repo, target):
+    if not target in ('deb', 'rpm'):
+        return abort(404)
+    repo_url = 'git@github.com:%s/%s.git' % (username, repo)
+    print(BANNER)
+    print('Building %s for %s ...' % (target, repo_url))
+
+    stopover = tempfile.mkdtemp(prefix='pypack')
+    shellout("""
+        cd {stopover} && git clone {repo_url} &&
+        cd {repo} && fpm --verbose -s python -t {target} .""",
+             stopover=stopover, repo_url=repo_url, repo=repo, target=target)
+    src = iterfiles(stopover, fun=lambda fn: fn.endswith(target)).next()
+    basename = os.path.basename(src)
+    dst = os.path.join(app.static_folder, basename)
+    shellout('cp {src} {dst}', src=src, dst=dst)
+
+    return send_from_directory(app.static_folder, basename, as_attachment=True)
+
+
+@app.route("/pypi/<name>/<target>")
+def build_from_pypi(name, target):
+    """ Does not cache anything. Serves from static. """
+    if not target in ('deb', 'rpm'):
+        return abort(404)
+    print(BANNER)
+    print('Building %s for %s...' % (target, name))
+    
+    stopover = tempfile.mkdtemp(prefix='pypack')
+    shellout('cd {stopover} && fpm --verbose -s python -t {target} {name}',
+             stopover=stopover, name=name, target=target)
+    src = iterfiles(stopover).next()
+    basename = os.path.basename(src)
+    dst = os.path.join(app.static_folder, basename)
+    shellout('cp {src} {dst}', src=src, dst=dst)
+
+    return send_from_directory(app.static_folder, basename, as_attachment=True)
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
