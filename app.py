0412a812d04acc7dec9b37217ac22922d62aa0b5
Martin Czygan <martin.czygan@gmail.com>
2014-05-15 16:41:11 +0200

    allow freeform input and downloads

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 # coding: utf-8
 
-from flask import Flask, render_template, redirect, url_for, abort, send_from_directory
+from flask import Flask, render_template, redirect, url_for, abort, send_from_directory, request
 from gluish.path import iterfiles
 from gluish.utils import shellout
 import hashlib
 import logging
 import os
 import shelve
+import shutil
 import tempfile
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
@@ -16,10 +17,9 @@ app = Flask(__name__)
 CACHE = os.path.join(app.static_folder, '.cache.shelve')
 
 
-@app.route("/github/<username>/<repo>/<target>")
-def build_from_github(username, repo, target):
-    if not target in ('deb', 'rpm'):
-        return abort(404)
+def github_clone_and_build(username, repo, target='deb'):
+    """ Clone a repo (username, repo) and build the `target` package.
+    Returns the filename, that is placed directly under `static`. """
     repo_url = 'git@github.com:%s/%s.git' % (username, repo)
 
     cache_key = hashlib.sha1('%s:%s' % (repo_url, target)).hexdigest()
@@ -36,13 +36,37 @@ def build_from_github(username, repo, target):
         basename = os.path.basename(src)
         dst = os.path.join(app.static_folder, basename)
         shellout('cp {src} {dst}', src=src, dst=dst)
+        shutil.rmtree(stopover)
         cache[cache_key] = basename
     else:
         logger.debug('Cache hit...')
-
     filename = cache[cache_key]
     cache.close()
+    return filename
 
+
+@app.route("/q", methods=["POST"])
+def build_from_user_input():
+    github_id = request.form.get('ghid')
+    username, repo = github_id.split('/', 1)
+    try:
+        filename = github_clone_and_build(username, repo, 'deb')
+    except RuntimeError as err:
+        logger.error("Could not build %s" % repo_url)
+        return abort(404)
+    return send_from_directory(app.static_folder, filename, as_attachment=True,
+                               attachment_filename=filename)
+
+
+@app.route("/github/<username>/<repo>/<target>")
+def build_from_github(username, repo, target):
+    if not target in ('deb', 'rpm'):
+        return abort(404)
+    try:
+        filename = github_clone_and_build(username, repo, target)
+    except RuntimeError as err:
+        logger.error("Could not build %s" % repo_url)
+        return abort(404)
     return send_from_directory(app.static_folder, filename, as_attachment=True,
                                attachment_filename=filename)
 
@@ -65,6 +89,7 @@ def build_from_pypi(name, target):
         basename = os.path.basename(src)
         dst = os.path.join(app.static_folder, basename)
         shellout('cp {src} {dst}', src=src, dst=dst)
+        shutil.rmtree(stopover)
         cache[cache_key] = basename
     else:
         logger.debug('Cache hit...')
