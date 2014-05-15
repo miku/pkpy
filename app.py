05377565b25a8514d3e68332a80881409a36e6d3
Martin Czygan <martin.czygan@gmail.com>
2014-05-15 11:14:17 +0200

    caching

--- app.py
+++ app.py
@@ -25,19 +25,19 @@ def build_from_github(username, repo, target):
     cache = shelve.open(CACHE)
 
     if not cache_key in cache:
-	logger.debug('Building (%s, %s) ...' % (repo_url, target))
-	stopover = tempfile.mkdtemp(prefix='borghq')
-	shellout("""
-	    cd {stopover} && git clone {repo_url} &&
-	    cd {repo} && fpm --verbose -s python -t {target} .""",
-		 stopover=stopover, repo_url=repo_url, repo=repo, target=target)
-	src = iterfiles(stopover, fun=lambda fn: fn.endswith(target)).next()
-	basename = os.path.basename(src)
-	dst = os.path.join(app.static_folder, basename)
-	shellout('cp {src} {dst}', src=src, dst=dst)
-	cache[cache_key] = basename
+        logger.debug('Building (%s, %s) ...' % (repo_url, target))
+        stopover = tempfile.mkdtemp(prefix='borghq')
+        shellout("""
+            cd {stopover} && git clone {repo_url} &&
+            cd {repo} && fpm --verbose -s python -t {target} .""",
+                 stopover=stopover, repo_url=repo_url, repo=repo, target=target)
+        src = iterfiles(stopover, fun=lambda fn: fn.endswith(target)).next()
+        basename = os.path.basename(src)
+        dst = os.path.join(app.static_folder, basename)
+        shellout('cp {src} {dst}', src=src, dst=dst)
+        cache[cache_key] = basename
     else:
-	logger.debug('Cache hit...')
+        logger.debug('Cache hit...')
 
     filename = cache[cache_key]
     cache.close()
@@ -50,22 +50,31 @@ def build_from_pypi(name, target):
     """ Does not cache anything. Serves from static. """
     if not target in ('deb', 'rpm'):
         return abort(404)
-    print(BANNER)
-    print('Building %s for %s...' % (target, name))
-    
-    stopover = tempfile.mkdtemp(prefix='pypack')
-    shellout('cd {stopover} && fpm --verbose -s python -t {target} {name}',
-             stopover=stopover, name=name, target=target)
-    src = iterfiles(stopover).next()
-    basename = os.path.basename(src)
-    dst = os.path.join(app.static_folder, basename)
-    shellout('cp {src} {dst}', src=src, dst=dst)
 
-    return send_from_directory(app.static_folder, basename, as_attachment=True)
+    cache_key = '%s %s' % (name, target)
+    cache = shelve.open(CACHE)
+
+    if not cache_key in cache:
+        logger.debug(   'Building %s for %s...' % (target, name))
+        stopover = tempfile.mkdtemp(prefix='pypack')
+        shellout('cd {stopover} && fpm --verbose -s python -t {target} {name}',
+                 stopover=stopover, name=name, target=target)
+        src = iterfiles(stopover).next()
+        basename = os.path.basename(src)
+        dst = os.path.join(app.static_folder, basename)
+        shellout('cp {src} {dst}', src=src, dst=dst)
+        cache[cache_key] = basename
+    else:
+        logger.debug('Cache hit...')
+
+    filename = cache[cache_key]
+    cache.close()
+
+    return send_from_directory(app.static_folder, filename, as_attachment=True)
 
 @app.route("/")
 def hello():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
