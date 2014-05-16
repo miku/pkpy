ad3c45503f2497251ad58ac50dcfd50dc174d7a5
Martin Czygan <martin.czygan@gmail.com>
2014-05-16 11:20:30 +0200

    added radio buttons for package type; added quota check

--- app.py
+++ app.py
@@ -25,19 +25,19 @@ PACKAGE_QUOTA = 10000000
 @app.before_request
 def ensure_package_cache():
     if not os.path.exists(PACKAGE_CACHE):
-	os.makedirs(PACKAGE_CACHE)
+        os.makedirs(PACKAGE_CACHE)
 
 
 @app.before_request
 def abort_on_overquota(quota=PACKAGE_QUOTA, directory=PACKAGE_CACHE):
     total = 0
     for filename in os.listdir(directory):
-	total += os.path.getsize(os.path.join(directory, filename))
+        total += os.path.getsize(os.path.join(directory, filename))
     if total > quota:
-	logger.error('OVERQUOTA: %s/%s' % (total, quota))
-	return abort(503)
+        logger.error('OVERQUOTA: %s/%s' % (total, quota))
+        return abort(503)
     else:
-	logger.debug('QUOTA OK: %s/%s' % (total, quota))
+        logger.debug('QUOTA OK: %s/%s' % (total, quota))
 
 
 @app.errorhandler(404)
@@ -62,7 +62,7 @@ def github_clone_and_build(username, repo, target='deb'):
                  stopover=stopover, repo_url=repo_url, repo=repo, target=target)
         src = iterfiles(stopover, fun=lambda fn: fn.endswith(target)).next()
         basename = os.path.basename(src)
-	dst = os.path.join(PACKAGE_CACHE, basename)
+        dst = os.path.join(PACKAGE_CACHE, basename)
         shellout('cp {src} {dst}', src=src, dst=dst)
         shutil.rmtree(stopover)
         cache[cache_key] = basename
@@ -77,14 +77,14 @@ def github_clone_and_build(username, repo, target='deb'):
 def build_from_user_input():
     target = request.form.get('target', 'deb')
     if not target in ('deb', 'rpm'):
-	return abort(404)
+        return abort(404)
     github_id = request.form.get('ghid')
     username, repo = github_id.split('/', 1)
     try:
-	filename = github_clone_and_build(username, repo, target)
+        filename = github_clone_and_build(username, repo, target)
     except RuntimeError as err:
-	logger.error("Could not find or build %s for %s/%s: %s" % (
-		     target, username, repo, err))
+        logger.error("Could not find or build %s for %s/%s: %s" % (
+                     target, username, repo, err))
         return abort(404)
     return send_from_directory(PACKAGE_CACHE, filename, as_attachment=True,
                                attachment_filename=filename)
@@ -97,7 +97,7 @@ def build_from_github(username, repo, target):
     try:
         filename = github_clone_and_build(username, repo, target)
     except RuntimeError as err:
-	logger.error("Could not build %s: %s" % (repo_url, err))
+        logger.error("Could not build %s: %s" % (repo_url, err))
         return abort(404)
     return send_from_directory(PACKAGE_CACHE, filename, as_attachment=True,
                                attachment_filename=filename)
@@ -120,7 +120,7 @@ def build_from_pypi(name, target):
                      stopover=stopover, name=name, target=target)
             src = iterfiles(stopover).next()
             basename = os.path.basename(src)
-	    dst = os.path.join(PACKAGE_CACHE, basename)
+            dst = os.path.join(PACKAGE_CACHE, basename)
             shellout('cp {src} {dst}', src=src, dst=dst)
             shutil.rmtree(stopover)
             cache[cache_key] = basename
